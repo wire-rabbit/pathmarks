55209c644621bdfbc3abb629c19fd859b8391a07
Jeremy Bensman <jeremy@wirerabbit.com>
2023-12-15 15:02:38 -0800

    Poetry config with empty textual app

---
+++ pathmarks/main.py
@@ -0,0 +1,17 @@
+from textual.app import App, ComposeResult
+from textual.widgets import Header, Footer
+
+
+class PathmarksApp(App):
+    """A Textual app for viewing/searching logfiles."""
+
+    def compose(self) -> ComposeResult:
+        """Create the app widgets."""
+        yield Header()
+        yield Footer()
+
+
+def start():
+    """Launch the app."""
+    app = PathmarksApp()
+    app.run()
